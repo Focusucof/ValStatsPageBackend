bbe9995d786eb2beba0342b76d4ddb41c2feb6d0
Focusucof <imthefirstfocus@gmail.com>
2021-11-17 10:07:39 -0500

    added express server

--- src/index.ts
+++ src/index.ts
@@ -2,13 +2,42 @@ import axios, { AxiosInstance } from 'axios';
 import login from './login';
 import getMatchHistory from './matchHistory';
 import { userDetails } from './globals';
-import { config } from 'dotenv';
+import * as dotenv from 'dotenv';
+import express from 'express';
+import cors from 'cors';
 
-config();
+const app = express();
+app.use(cors());
+var userDetails: userDetails;
 
-async function main() {    
+app.get('/', async function (req, res) {
     //@ts-ignore
-    const userDetails: userDetails = await login(process.env.USERNAME, process.env.PASSWORD);
+    userDetails = await login();
+    res.sendStatus(200);
+});
+
+app.get('/match-history', async function(req, res) {
+    const pdRequest: AxiosInstance = axios.create({
+        baseURL: 'https://pd.na.a.pvp.net/',
+        method: 'GET',
+        headers: {
+            'X-Riot-Entitlements-JWT': userDetails.entitlements,
+            'X-Riot-ClientPlatform': 'ew0KCSJwbGF0Zm9ybVR5cGUiOiAiUEMiLA0KCSJwbGF0Zm9ybU9TIjogIldpbmRvd3MiLA0KCSJwbGF0Zm9ybU9TVmVyc2lvbiI6ICIxMC4wLjE5MDQyLjEuMjU2LjY0Yml0IiwNCgkicGxhdGZvcm1DaGlwc2V0IjogIlVua25vd24iDQp9',
+            'Authorization': 'Bearer ' + userDetails.access_token
+        }
+    });
+
+    let matchHistory = await getMatchHistory(userDetails.puuid, pdRequest);
+    res.send(matchHistory);
+});
+
+app.listen(1337, () => {
+    console.log('listening on port 3000');
+});
+
+async function main() {   
+    //@ts-ignore
+    const userDetails: userDetails = await login();
 
     const pdRequest: AxiosInstance = axios.create({
         baseURL: 'https://pd.na.a.pvp.net/',
@@ -24,4 +53,4 @@ async function main() {
     console.log(matchHistory);
 }
 
-main();
+//main();
--- src/matchHistory.ts
+++ src/matchHistory.ts
@@ -1,10 +1,11 @@
-import axios, { Axios, AxiosInstance } from 'axios';
+import axios, { AxiosInstance } from 'axios';
 
 export default async function getMatchHistory(puuid: string, pdRequest: AxiosInstance) {
     var matchHistory;
     var matchIDs: string[] = [];
     var matchDetails: any[] = [];
 
+    // Get match ids of last 5 competitive games
     await pdRequest.get(`match-history/v1/history/${puuid}?startIndex=0&endIndex=5&queue=competitive`).then(response => {
         matchHistory = response.data.History;
         for(let i = 0; i < matchHistory.length; i++) {
@@ -13,6 +14,7 @@ export default async function getMatchHistory(puuid: string, pdRequest: AxiosIns
     });
     
     for(let i = 0; i < matchIDs.length; i++) {
+        // Get match details and parse it down to my player and team scores only
         await pdRequest.get(`match-details/v1/matches/${matchIDs[i]}`).then(response => {
             for(let j = 0; j < response.data.players.length; j++) {
                 if(response.data.players[j].subject == puuid) {
@@ -25,6 +27,7 @@ export default async function getMatchHistory(puuid: string, pdRequest: AxiosIns
                             deaths: response.data.players[j].stats.deaths,
                             assists: response.data.players[j].stats.assists,
                             score: response.data.players[j].stats.score,
+                            agent: response.data.players[j].characterId
                         };
                     } else {
                         gameinfo = {
@@ -34,6 +37,7 @@ export default async function getMatchHistory(puuid: string, pdRequest: AxiosIns
                             deaths: response.data.players[j].stats.deaths,
                             assists: response.data.players[j].stats.assists,
                             score: response.data.players[j].stats.score,
+                            agent: response.data.players[j].characterId
                         };
                     }
                     matchDetails.push(gameinfo);
